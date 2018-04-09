ece5adb9320b6a175f6cd63f2dfe0a1432e41f65
freebirdweij <freebirdweij@foxmail.com>
2018-04-09 10:36:31 +0800

    Add two wavelet methods.

--- code/com/freebirdweij/goldanalyse/ml/wavelet.py
+++ code/com/freebirdweij/goldanalyse/ml/wavelet.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import argparse
 import numpy as np
-
+import pywt
 
 from numpy import shape
 from scipy import linalg
@@ -48,10 +48,77 @@ def construct_sequence_mat(seqData,seqLen):
         seqMat.append(row)
         
     return seqMat
+
+#pywt.families()
+#['haar', 'db', 'sym', 'coif', 'bior', 'rbio', 'dmey', 'gaus', 'mexh', 'morl', 'cgau', 'shan', 'fbsp', 'cmor']
+"""
+haar family: haar
+db family: db1, db2, db3, db4, db5, db6, db7, db8, db9, db10, db11, db12, db13, db14, db15, db16, db17, db18, db19, db20, db21, db22, db23, db24, db25, db26, db27, db28, db29, db30, db31, db32, db33, db34, db35, db36, db37, db38
+sym family: sym2, sym3, sym4, sym5, sym6, sym7, sym8, sym9, sym10, sym11, sym12, sym13, sym14, sym15, sym16, sym17, sym18, sym19, sym20
+coif family: coif1, coif2, coif3, coif4, coif5, coif6, coif7, coif8, coif9, coif10, coif11, coif12, coif13, coif14, coif15, coif16, coif17
+bior family: bior1.1, bior1.3, bior1.5, bior2.2, bior2.4, bior2.6, bior2.8, bior3.1, bior3.3, bior3.5, bior3.7, bior3.9, bior4.4, bior5.5, bior6.8
+rbio family: rbio1.1, rbio1.3, rbio1.5, rbio2.2, rbio2.4, rbio2.6, rbio2.8, rbio3.1, rbio3.3, rbio3.5, rbio3.7, rbio3.9, rbio4.4, rbio5.5, rbio6.8
+dmey family: dmey
+gaus family: gaus1, gaus2, gaus3, gaus4, gaus5, gaus6, gaus7, gaus8
+mexh family: mexh
+morl family: morl
+cgau family: cgau1, cgau2, cgau3, cgau4, cgau5, cgau6, cgau7, cgau8
+shan family: shan
+fbsp family: fbsp
+cmor family: cmor
+"""
+#print(pywt.Modes.modes)
+#['zero', 'constant', 'symmetric', 'periodic', 'smooth', 'periodization', 'reflect']
+def dwt_single_level(seqMat,waveObj,waveMode):
+    dwtMat = []
+    for row in seqMat:
+      dwtRow = []
+      cA, cD = pywt.dwt(row, wavelet=waveObj, mode=waveMode)
+      dwtRow.append(cA)
+      dwtRow.append(cD)
+      dwtMat.append(dwtRow)
+    return dwtRow
   
-def wavelet_decompose(seqMat):
-    
+def dwt_multi_level(seqMat,waveObj,waveMode,waveLevel):
+    if waveLevel == 1 :
+      return dwt_single_level(seqMat,waveObj,waveMode)
+    elif waveLevel == 2 :
+      dwtMat = []
+      for row in seqMat:
+        dwtRow = []
+        cA2, cD2, cD1 = pywt.wavedec(row, waveObj, mode=waveMode, level=2)
+        dwtRow.append(cA2)
+        dwtRow.append(cD2)
+        dwtRow.append(cD1)
+        dwtMat.append(dwtRow)
+      return dwtMat
+    elif waveLevel == 3 :
+      dwtMat = []
+      for row in seqMat:
+        dwtRow = []
+        cA3, cD3,cD2, cD1 = pywt.wavedec(row, waveObj, mode=waveMode, level=3)
+        dwtRow.append(cA3)
+        dwtRow.append(cD3)
+        dwtRow.append(cD2)
+        dwtRow.append(cD1)
+        dwtMat.append(dwtRow)
+      return dwtMat
+    elif waveLevel == 4 :
+      dwtMat = []
+      for row in seqMat:
+        dwtRow = []
+        cA4, cD4,cD3,cD2, cD1 = pywt.wavedec(row, waveObj, mode=waveMode, level=4)
+        dwtRow.append(cA4)
+        dwtRow.append(cD4)
+        dwtRow.append(cD3)
+        dwtRow.append(cD2)
+        dwtRow.append(cD1)
+        dwtMat.append(dwtRow)
+      return dwtMat
+    else :
+      return 
     return
+  
 def main():
     
   DATA_INPUTS = 'audt365-2018-2-28-day.csv'
