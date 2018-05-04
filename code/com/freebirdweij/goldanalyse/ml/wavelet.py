9158461b8f2aa9af32ab95264b9c4a85157ae219
freebirdweij <freebirdweij@foxmail.com>
2018-05-04 23:24:26 +0800

    Update training records.

--- code/com/freebirdweij/goldanalyse/ml/wavelet.py
+++ code/com/freebirdweij/goldanalyse/ml/wavelet.py
@@ -122,7 +122,7 @@ def dwt_multi_level(seqMat,waveObj,waveMode,waveLevel):
   
 def main():
     
-  DATA_INPUTS = 'hjxh365-2018-4-16-day-clear-full.csv'
+  DATA_INPUTS = 'hjxh365-2018-4-16-day-plus-norm-clear-pre-wavelet.csv'
 
   input_datas = base.load_csv_without_header(DATA_INPUTS,target_dtype=np.float32,
                                   features_dtype=np.float32,target_column=0)
@@ -132,11 +132,11 @@ def main():
   seqMat = construct_sequence_mat(dataMat,60)
   print('seqMat:-----------------------')
   print(seqMat)
-  base.write_a_dataset_to_a_csv('hjxh365-2018-4-16-day-clear-full-seq.csv', seqMat)
+  base.write_a_dataset_to_a_csv('hjxh365-2018-4-16-day-plus-norm-clear-pre-wavelet-seq.csv', seqMat)
 
 #  dwtMat = dwt_single_level(seqMat,'db2','symmetric')
   dwtMat = dwt_multi_level(seqMat,'db1','symmetric',4)
-  base.write_a_dataset_to_a_csv('hjxh365-2018-4-16-day-clear-full-dwt-ml4.csv', dwtMat)
+  base.write_a_dataset_to_a_csv('hjxh365-2018-4-16-day-plus-norm-clear-dwt-ml4.csv', dwtMat)
 
 
 if __name__ == '__main__':
